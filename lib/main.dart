6ea09612d8e60d422d5d8a527640e2d4be035eec
maaaaaaaaad <integral5137@gmail.com>
2023-08-03 20:34:28 +0900

    e

--- lib/main.dart
+++ lib/main.dart
@@ -15,71 +15,93 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'home'),
+      home: const Look(title: 'home'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Look extends StatefulWidget {
+  const Look({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Look> createState() => _Look();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _dicrementCounter() {
-    setState(() {
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
-
+class _Look extends State<Look> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
+    return const Scaffold(
+      body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 20),
-                    backgroundColor: const Color.fromARGB(31, 4, 45, 227)),
-                onPressed: _dicrementCounter,
-                child: const Text('-'),
-              ),
-              const SizedBox(height: 30),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ));
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[Text("hello")],
+      )),
+    );
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   void _dicrementCounter() {
+//     setState(() {
+//       if (_counter > 0) {
+//         _counter--;
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           centerTitle: true,
+//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//           title: Text(widget.),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               const Text(
+//                 'You have pushed the button this many times:',
+//               ),
+//               Text(
+//                 '$_counter',
+//                 style: Theme.of(context).textTheme.headlineMedium,
+//               ),
+//               TextButton(
+//                 style: TextButton.styleFrom(
+//                     textStyle: const TextStyle(fontSize: 20),
+//                     backgroundColor: const Color.fromARGB(31, 4, 45, 227)),
+//                 onPressed: _dicrementCounter,
+//                 child: const Text('-'),
+//               ),
+//               const SizedBox(height: 30),
+//             ],
+//           ),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: _incrementCounter,
+//           tooltip: 'Increment',
+//           child: const Icon(Icons.add),
+//         ));
+//   }
+// }
