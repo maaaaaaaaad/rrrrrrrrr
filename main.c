7b5b1154f4eba645186a896a8c0f43b0f7f15579
mad <mad@madui-MacBookPro.local>
2025-05-05 14:33:08 +0900

    e

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    const int arr[4] = {1, 2, 3, 4};
+
+    const int *arr_ptr_fir = arr;
+    const int *arr_ptr_sec = arr_ptr_fir;
+
+    arr_ptr_sec++;
+
+    printf("first arr index: %d\n", *arr_ptr_fir);
+    printf("second arr index: %d\n", *arr_ptr_sec);
     return 0;
 }
